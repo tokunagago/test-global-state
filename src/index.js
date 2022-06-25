eec4feadf6e12a9ce4c1869255ad84723767458d
tokunagago <qazmlpplmzaq5@gmail.com>
2022-06-25 19:09:10 +0900

    Context 追加

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AdminFlagProvider } from './components/providers/AdminFlagProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AdminFlagProvider>
     <App />
-  </React.StrictMode>
+  </AdminFlagProvider>
 );
